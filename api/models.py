19431619ac8ff3289d14d1baa9ad0aa94795c297
malekradwan1300 <malekradwan13.00@gmail.com>
2025-07-30 16:14:14 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -8,6 +8,8 @@ model_proxy = {
     "gpt-4-turbo": "gpt-4-turbo-2024-04-09",
     "gpt-4o": "gpt-4o-2024-08-06",
     "gpt-4o-mini": "gpt-4o-mini-2024-07-18",
+    "gpt-4-1-mini": "gpt-4-1-mini",
+    "auto": "auto",           # بالنسبة لـ gpt 4.5
     "o1-preview": "o1-preview-2024-09-12",
     "o1-mini": "o1-mini-2024-09-12",
     "o1": "o1-2024-12-18",
@@ -19,13 +21,19 @@ model_proxy = {
 }
 
 model_system_fingerprint = {
-    "gpt-3.5-turbo-0125": ["fp_b28b39ffa8"],
-    "gpt-3.5-turbo-1106": ["fp_592ef5907d"],
-    "gpt-4-0125-preview": ["fp_f38f4d6482", "fp_2f57f81c11", "fp_a7daf7c51e", "fp_a865e8ede4", "fp_13c70b9f70",
-                           "fp_b77cb481ed"],
-    "gpt-4-1106-preview": ["fp_e467c31c3d", "fp_d986a8d1ba", "fp_99a5a401bb", "fp_123d5a9f90", "fp_0d1affc7a6",
-                           "fp_5c95a4634e"],
-    "gpt-4-turbo-2024-04-09": ["fp_d1bac968b4"],
-    "gpt-4o-2024-05-13": ["fp_3aa7262c27"],
-    "gpt-4o-mini-2024-07-18": ["fp_c9aa9c0491"]
+    "gpt-3.5-turbo-0125": ["b28b39ffa8"],
+    "gpt-3.5-turbo-1106": ["592ef5907d"],
+    "gpt-4-0125-preview": [
+        "f38f4d6482", "2f57f81c11", "a7daf7c51e",
+        "a865e8ede4", "13c70b9f70", "b77cb481ed"
+    ],
+    "gpt-4-1106-preview": [
+        "e467c31c3d", "d986a8d1ba", "99a5a401bb",
+        "123d5a9f90", "0d1affc7a6", "5c95a4634e"
+    ],
+    "gpt-4-turbo-2024-04-09": ["d1bac968b4"],
+    "gpt-4o-2024-05-13": ["3aa7262c27"],
+    "gpt-4o-mini-2024-07-18": ["c9aa9c0491"],
+    "gpt-4-1-mini": ["unknown"],
+    "auto": []  # مضاف بدون بصمة
 }
